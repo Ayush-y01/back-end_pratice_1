4392d2b1221a93b41df2a858d3395df0a0205646
ayushyadav8900@gmail.com <ayushyadav8900@gmail.com>
2026-07-22 20:56:16 +0530

    create payment system

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -97,14 +97,15 @@ export const addTodo = async (req, res, next) => {
     }
 
     const todo = await Todo.create({
+        user,
         title,
         task
     })
 
     return res.status(201).json({
         message:"todo create successfully",
-        id: req.user._id,
-        todo
+        title,
+        user: req.user.id
     })
 }
 
@@ -126,4 +127,63 @@ export const deleteTodo = async (req, res, next) => {
     return res.status(200).json({
         message:"Todo Deleted successfully"
     })
+}
+
+export const updateTodo = async (req,res, next) => {
+    const user = req.user
+    
+    if (!user) {
+        return res.status(402).json({
+            message:"please login first"
+        })        
+    }
+    
+    const  todoId = req.params.id
+    const {title, task} = req.body
+
+    const todo = await Todo.findOneAndUpdate(
+        {_id:todoId,user:user.id,},
+        {title, task},
+        {new:true, runValidators:true}
+    )
+
+    return res.status(202).json({
+        message:"user Update successfully",
+        todo
+    })
+}
+
+
+export const searchTodo = async (req, res,next) => {
+    const user = req.user
+    
+    if (!user) {
+        return res.status(401).json({
+            message:"Please login first"
+        })
+    }
+
+    const { q } = req.query
+
+    try {
+        if (!q) {
+            return res.status(400).json({
+                message:"Search required query"
+        })
+        }
+
+
+        const search = await Todo.find({
+            user:user.id,
+            title:{$regex:q, $options:"i"}
+        })
+
+        return res.status(200).json({
+            success:true,
+            count: search.length,
+            todos:search
+        })
+    } catch (error) {
+        next(error)
+    }
 }
