ce346b4bc5a9111973695c39b271d3b8523b7fa2
Ayush yadav <ayushyadav8900@gmail.com>
2026-07-27 12:57:13 +0000

    some updates

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios"
 import {server} from "../main"
+import { FaPen, FaTrash } from "react-icons/fa";
+import { MdCheckBox, MdCheckBoxOutlineBlank } from 'react-icons/md';
 
 const Home = () => {
     const isAuth  = false
@@ -11,6 +13,9 @@ const Home = () => {
     const [task, setTask] = useState("")
     const [isOpen, setIsOpen] = useState(false)
     const [addTodo, setAddTodo] = useState(null)
+    const [isCheck, setIsCheck ] = useState(false)
+
+    const handletoggle = () => setIsCheck(!isCheck);
 
     useEffect(() => {
         const getTodos = async () => {
@@ -54,10 +59,10 @@ const Home = () => {
         }
     }
 
-    const handleSubmit = (e) => {
-        setTitle(e.target.value)
-        setTask(e.target.value)
-    }
+    // const handleSubmit = (e) => {
+    //     setTitle(e.target.value)
+    //     setTask(e.target.value)
+    // }
 
 
 
@@ -86,31 +91,56 @@ const Home = () => {
                 <div className=" rounded">
                     <div className="justify-end flex ">
                 {isOpen ? (
-                    <div className="bg-blue-100">
-                        <label>Titel</label>
-                        <input type="text" value={title} onChange={handleSubmit} />
-                        <label>Description</label>
-                        <input type="text" value={task} onChange={handleSubmit} />
-                        <input type="submit" onClick={() => addTodoApi()} />
+                    <div className="bg-gray-200 rounded mb-4 ">
+                        <div className="flex justify-end">
+                            <button className=" font-semibold px-3 text-red-500" onClick={() => setIsOpen(false)}>x</button>
+                        </div>
+                        <div className="p-8">
+                            <label className="px-9">Titel</label>
+                        <input type="text" className="bg-white rounded mb-2" value={title} onChange={((e) => setTitle(e.target.value) )} />
+                        <label className="px-2">Description</label>
+                        <textarea className="bg-white rounded px-4" type="text" value={task} onChange={((e) => setTask(e.target.value))} ></textarea>
+                        <input type="submit" className="ml-66 mt-2 justify-end px-2 bg-blue-500 text-white p-1 rounded" onClick={() => addTodoApi()} />
+                        </div>
                     </div>
-                ):(
-                    <div>
-                        <button className="bg-blue-200 rounded-lg p-1 mb-2 shadow" onClick={() => setIsOpen(true)}>Add Todo</button>
-                    </div>
-                )}
+                 ):( 
+                   <div> 
+                         <button className="bg-blue-500 text-white rounded-lg px-2 py-1 mb-2 shadow" onClick={() => setIsOpen(true)}>Add Todo</button> 
+                     </div> 
+                 )} 
 
             </div>
                     <h2 className="font-bold bg-white shadow mb-4 text-4xl px-10 py-6 rounded-lg">Todo's</h2>
 
                 <div className="px-10 font-semibold mt-1 bg-white shadow py-4 rounded-lg">
-                    <h4>Title</h4>
+                    <div className="flex justify-between py-2">
+                        <h4>Title</h4>
+                        <div className="flex gap-4 items-center">
+                            <FaPen className="" />
+                            <FaTrash />
+                           {isCheck ? (
+        <MdCheckBox color="#007bff" />
+      ) : (
+        <MdCheckBoxOutlineBlank color="#6c757d" />
+      )}
+                        </div>
+                    </div>
                     <p>Task</p>
-                    
                 </div>
                 <div className="px-10 font-semibold mt-1 bg-white shadow py-4 rounded-lg">
-                    <h4>Title</h4>
+                   <div className="flex justify-between py-2">
+                        <h4>Title</h4>
+                        <div className="flex gap-4 items-center">
+                            <FaPen className="" />
+                            <FaTrash />
+                            {isCheck ? (
+        <MdCheckBox color="#007bff" />
+      ) : (
+        <MdCheckBoxOutlineBlank color="#6c757d" />
+      )}
+                        </div>
+                    </div>
                     <p>Task</p>
-                    
                 </div>
                 </div>
             </div>
