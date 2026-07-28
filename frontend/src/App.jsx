cb3f3393d74ce062c4981a4b97da3da9273bf51d
Ayush yadav <ayushyadav8900@gmail.com>
2026-07-28 13:00:01 +0000

    set update and delete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
+// import Login from "./pages/Login"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <Navbar />  
-      <Home />
-    </div>
+    <BrowserRouter> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/login" element={<Login />} /> */}
+      </Routes>
+    </BrowserRouter>
   )
 }
 
