e4e5e65e8285a5f0c91cb79343f93ff879c9db44
ayushyadav8900@gmail.com <ayushyadav8900@gmail.com>
2026-08-02 20:36:07 +0530

    some changes

--- server/index.js
+++ server/index.js
@@ -3,16 +3,22 @@ import dotenv from "dotenv"
 import connectDB from "./src/DB/connection.js"
 import UserRouter from "./src/routes/user.js"
 import TodoRouter from "./src/routes/todo.js"
+import cors from "cors"
 
 dotenv.config()
+
 
 connectDB()
 
 const app = express();
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(express.json())
 
-app.use("/user",UserRouter)
-app.use("/todo",TodoRouter)
+app.use("/user", UserRouter)
+app.use("/todo", TodoRouter)
 
 
 const PORT = process.env.PORT || 3000
